7acb2b5b8127fe9852ba4800fe9c2170e16e13f4
dxntlxxkbxck <dxntlxxkbxck@gmail.com>
2026-05-27 17:08:26 +0300

    step 5

---
+++ src/games/even.js
@@ -0,0 +1,36 @@
+import readlineSync from 'readline-sync';
+
+const roundsCount = 3;
+
+const getRandomNumber = () => Math.floor(Math.random() * 100) + 1;
+
+const isEven = (number) => number % 2 === 0;
+
+const playEvenGame = () => {
+  console.log('Welcome to the Brain Games!');
+
+  const name = readlineSync.question('May I have your name? ');
+  console.log(`Hello, ${name}!`);
+
+  console.log('Answer "yes" if the number is even, otherwise answer "no".');
+
+  for (let i = 0; i < roundsCount; i += 1) {
+    const number = getRandomNumber();
+    const correctAnswer = isEven(number) ? 'yes' : 'no';
+
+    console.log(`Question: ${number}`);
+    const userAnswer = readlineSync.question('Your answer: ');
+
+    if (userAnswer !== correctAnswer) {
+      console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.`);
+      console.log(`Let's try again, ${name}!`);
+      return;
+    }
+
+    console.log('Correct!');
+  }
+
+  console.log(`Congratulations, ${name}!`);
+};
+
+export default playEvenGame;
