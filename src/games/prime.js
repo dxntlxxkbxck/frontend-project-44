ad430788911d72dfbf03ea287fdc30e3ad5ff740
dxntlxxkbxck <dxntlxxkbxck@gmail.com>
2026-05-28 13:32:42 +0300

    step 9

---
+++ src/games/prime.js
@@ -0,0 +1,32 @@
+import runGame from '../index.js';
+
+const description = 'Answer "yes" if given number is prime. Otherwise answer "no".';
+
+const getRandomNumber = () => Math.floor(Math.random() * 100) + 1;
+
+const isPrime = (number) => {
+  if (number < 2) {
+    return false;
+  }
+
+  for (let i = 2; i <= Math.sqrt(number); i += 1) {
+    if (number % i === 0) {
+      return false;
+    }
+  }
+
+  return true;
+};
+
+const getRoundData = () => {
+  const number = getRandomNumber();
+  const correctAnswer = isPrime(number) ? 'yes' : 'no';
+
+  return [number, correctAnswer];
+};
+
+const runPrimeGame = () => {
+  runGame(description, getRoundData);
+};
+
+export default runPrimeGame;
